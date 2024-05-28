17b791c9239a41543b142dbc087f24dc20510b67
Sylwester Mielniczuk <flaboy.com@gmail.com>
2024-05-28 20:26:34 +0100

    yolo.cx widgets modules test - cube.js

---
+++ projects/yolo.cx/cube/cube.js
@@ -0,0 +1,39 @@
+// static/libs/widgets/cube.js
+import * as THREE from 'https://unpkg.com/three@0.137.5/build/three.module.js';
+
+export function initWidget(scene) {
+
+
+
+    if (window.newCube) {
+        scene.remove(window.newCube);
+    }
+
+
+
+    const newGeometry = new THREE.BoxGeometry();
+    const newMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+    const newCube = new THREE.Mesh(newGeometry, newMaterial);
+
+    scene.add(newCube);
+    window.newCube = newCube;
+
+
+    function rotateNewCube(delta) {
+        newCube.rotation.x += delta * 0.5;
+        newCube.rotation.y += delta * 0.5;
+    }
+
+
+    if (!window.customUpdateFunctions) {
+        window.customUpdateFunctions = [];
+    }
+
+    window.customUpdateFunctions.push(rotateNewCube);
+
+    window.customUpdate = function(delta) {
+        window.customUpdateFunctions.forEach(func => func(delta));
+    };
+}
+
+
